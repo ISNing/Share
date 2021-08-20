60ec36facf937028e1de7dc59ab611eadb0caee7
isning <a1208733578@qq.com>
2021-08-20 15:52:24 +0800

    ss

---
+++ app/src/main/java/org/exthmui/share/base/events/SentEvent.java
@@ -0,0 +1,9 @@
+package org.exthmui.share.base.events;
+
+import java.util.EventObject;
+
+public class SentEvent extends EventObject {
+    public SentEvent(Object source) {
+        super(source);
+    }
+}
---
+++ app/src/main/java/org/exthmui/share/base/listeners/BaseEventListener.java
@@ -0,0 +1,27 @@
+package org.exthmui.share.base.listeners;
+
+import androidx.annotation.NonNull;
+
+import java.lang.reflect.Method;
+import java.util.EventListener;
+import java.util.EventObject;
+import java.util.HashMap;
+import java.util.Map;
+
+public interface BaseEventListener extends EventListener {
+    /**
+     * Like this:
+     * <code>
+     * HashMap<Class<? extends EventObject>, Method[]> EVENT_TYPES_ALLOWED =  new HashMap<Class<? extends EventObject>, Method[]>()
+     * {{
+     *      put(EventObject.class, new Method[]{,});
+     * }};
+     * default Map<Class<? extends EventObject>, Method[]> getEventTMethodMap(){
+            return this.EVENT_TYPES_ALLOWED == null ? new HashMap<>() : EVENT_TYPES_ALLOWED;
+     * };</code>
+     */
+    Map<Class<? extends EventObject>, Method[]> _getEventTMethodMap();
+    default Map<Class<? extends EventObject>, Method[]> getEventTMethodMap(){
+        return this._getEventTMethodMap() == null ? new HashMap<>() : this._getEventTMethodMap();
+    };
+}
