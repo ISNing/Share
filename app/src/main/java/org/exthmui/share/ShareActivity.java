1528736652d5bad35edaefe0699abf03b1aa840b
ISNing <a1208733578@qq.com>
2022-04-08 23:28:47 +0800

    sf

--- app/src/main/java/org/exthmui/share/ShareActivity.java
+++ app/src/main/java/org/exthmui/share/ShareActivity.java
@@ -1,11 +1,13 @@
 package org.exthmui.share;
 
 import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.exthmui.share.shared.FileUtils;
+import org.exthmui.share.services.DiscoverService;
 import org.exthmui.share.shared.base.Entity;
 import org.exthmui.share.shared.base.exceptions.FailedResolvingUriException;
 
@@ -20,11 +22,11 @@ public class ShareActivity extends AppCompatActivity {
         ShareBottomSheetFragment shareFragment = new ShareBottomSheetFragment();
         shareFragment.setEntities(getEntities());
         shareFragment.show(getSupportFragmentManager(), shareFragment.getTag());
+        grantUriPermissions();
     }
 
     private List<Entity> getEntities() {
         final List<Entity> entities = new ArrayList<>();
-        final String mimeType = getIntent().getType();
         final ClipData clipData = getIntent().getClipData();
         if (clipData != null) {
             for (int i = 0; i < clipData.getItemCount(); i++) {
@@ -43,7 +45,7 @@ public class ShareActivity extends AppCompatActivity {
         } else {
             Entity entity = null;
             try {
-                entity = new Entity(getApplicationContext(), getIntent().getData(), FileUtils.getFileTypeByMime(mimeType).getNumVal());
+                entity = new Entity(getApplicationContext(), getIntent().getData());
             } catch (FailedResolvingUriException e) {
                 e.printStackTrace();
             }
@@ -54,5 +56,28 @@ public class ShareActivity extends AppCompatActivity {
             }
         }
         return entities;
+    }
+
+    private void grantUriPermissions() {
+        final ClipData clipData = getIntent().getClipData();
+        if (clipData != null) {
+            grantUriPermissions(clipData);
+        } else {
+            grantUriPermissions(getIntent().getData());
+        }
+    }
+
+    private void grantUriPermissions(Uri data) {
+        Intent intent = new Intent(this, DiscoverService.class)
+                .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                .setData(data);
+        startService(intent);
+    }
+
+    private void grantUriPermissions(ClipData data) {
+        Intent intent = new Intent(this, DiscoverService.class)
+                .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setClipData(data);
+        startService(intent);
     }
 }
