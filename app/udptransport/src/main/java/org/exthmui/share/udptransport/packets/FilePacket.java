0ace2fe136ed2bad4032bccb6bb9fefb35702444
ISNing <33572057+ISNing@users.noreply.github.com>
2022-08-21 23:59:06 +0800

    Fix: wrong flag tip

--- app/udptransport/src/main/java/org/exthmui/share/udptransport/packets/FilePacket.java
+++ app/udptransport/src/main/java/org/exthmui/share/udptransport/packets/FilePacket.java
@@ -12,7 +12,7 @@ public final class FilePacket extends AbstractCommandPacket<FilePacket> {
 
     public static final int[] GROUP_ID_TIP = {0, 0};
     public static final int[] PACKET_ID_TIP = {1, 2};
-    public static final int[] FLAG_TIP = {3};
+    public static final int[] FLAG_TIP = {3, 3};
     public static final int[] DATA_TIP = {4};
 
     private FilePacket(DatagramPacket packet) {
