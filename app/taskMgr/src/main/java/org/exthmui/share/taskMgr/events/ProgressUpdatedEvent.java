dc286b8843520bfbeae6e38c792677545a1f6586
ISNing <a1208733578@qq.com>
2023-03-17 11:14:34 +0800

    taskMgr: ProgressUpdatedEvent: progress data should always be nonnull

--- app/taskMgr/src/main/java/org/exthmui/share/taskMgr/events/ProgressUpdatedEvent.java
+++ app/taskMgr/src/main/java/org/exthmui/share/taskMgr/events/ProgressUpdatedEvent.java
@@ -2,16 +2,20 @@ package org.exthmui.share.taskMgr.events;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+
 import java.util.EventObject;
 
 public class ProgressUpdatedEvent extends EventObject {
+    @NonNull
     private final Bundle progress;
 
-    public ProgressUpdatedEvent(Object source, Bundle progress) {
+    public ProgressUpdatedEvent(Object source, @NonNull Bundle progress) {
         super(source);
         this.progress = progress;
     }
 
+    @NonNull
     public Bundle getProgress() {
         return progress;
     }
