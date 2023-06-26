7bdd783af0fe5abc420c5fa92de56e8d49703e88
ISNing <1208733578@qq.com>
2023-06-26 16:34:23 +0800

    [DROP THIS BEFORE PUSH]Disable Appcenter

--- app/src/main/java/org/exthmui/share/ShareApplication.java
+++ app/src/main/java/org/exthmui/share/ShareApplication.java
@@ -3,21 +3,21 @@ package org.exthmui.share;
 import android.app.Application;
 import android.content.pm.PackageManager;
 
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
+//import com.microsoft.appcenter.AppCenter;
+//import com.microsoft.appcenter.analytics.Analytics;
+//import com.microsoft.appcenter.crashes.Crashes;
 
 public class ShareApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        try {
-            AppCenter.start(this, getPackageManager()
-                            .getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA)
-                            .metaData.getString("appCenterSecret"),
-                    Analytics.class, Crashes.class);
-        } catch (PackageManager.NameNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            AppCenter.start(this, getPackageManager()
+//                            .getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA)
+//                            .metaData.getString("appCenterSecret"),
+//                    Analytics.class, Crashes.class);
+//        } catch (PackageManager.NameNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
