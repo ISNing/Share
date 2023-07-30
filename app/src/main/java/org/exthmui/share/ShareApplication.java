ad23e75da6706d1b3667af55e76fad903a71054c
ISNing <1208733578@qq.com>
2023-07-30 10:51:32 +0800

    Share: Remove AppCenter

--- app/src/main/java/org/exthmui/share/ShareApplication.java
+++ app/src/main/java/org/exthmui/share/ShareApplication.java
@@ -4,22 +4,11 @@ import android.app.Application;
 
 import com.google.android.material.color.DynamicColors;
 
-//import com.microsoft.appcenter.AppCenter;
-//import com.microsoft.appcenter.analytics.Analytics;
-//import com.microsoft.appcenter.crashes.Crashes;
 
 public class ShareApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         DynamicColors.applyToActivitiesIfAvailable(this);
-//        try {
-//            AppCenter.start(this, getPackageManager()
-//                            .getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA)
-//                            .metaData.getString("appCenterSecret"),
-//                    Analytics.class, Crashes.class);
-//        } catch (PackageManager.NameNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
     }
 }
